5784a4ddcea6e8076d8b306619b648a55b02ab14
Capicapilla <javier.capicapilla@gmail.com>
2025-06-11 20:45:27 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+  //      1) should have items and length properties #add(x)
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+//      2) should add a single value to SortedList
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a -b);
+    this.length = this.items.length;
+  }
+  get(pos) {
+  if (pos >= this.length) {
+    throw new Error("OutOfBounds");
+  }
+  return this.items[pos];
+}
 
-  get(pos) {}
+  max() {
+  if (this.length === 0) {
+    throw new Error("EmptySortedList");
+  }
+  return this.items[this.length - 1];
+}
+  min() {
+  if (this.length === 0) {
+    throw new Error("EmptySortedList");
+  }
+  return this.items[0];
+}
 
-  max() {}
+  sum() {
+  return this.items.reduce((acc, val) => acc + val, 0);
+}
 
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  avg() {
+  if (this.length === 0) {
+    throw new Error("EmptySortedList");
+  }
+  return this.sum() / this.length;
+}
 }
 
 module.exports = SortedList;
